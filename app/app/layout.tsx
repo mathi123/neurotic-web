030c6ac246f4b06b985d6b7440949729c6969f5e
Mathias Colpaert <mathias.colpaert@trensition.eu>
2025-11-08 11:41:03 +0100

    feat: add theme switcher

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,9 +1,14 @@
 import '@/app/globals.css';
+import { ThemeProvider } from '@/app/components/theme-provider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
