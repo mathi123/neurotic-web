9d5e8c8269498b02883f0a7e4c62721152254d4c
Mathias Colpaert <mathias.colpaert@trensition.eu>
2025-11-11 16:01:12 +0100

    chore: remove AuthJS and models

--- app/actions/user/create.ts
+++
@@ -1,9 +0,0 @@
-import { saltAndHashPassword } from '@/actions/security/password';
-import { User } from '@/domain/user.model';
-import { dbUserCreate } from '@/storage/user/user.create';
-
-export const createUser = async (user: User, password: string): Promise<User> => {
-  const hashedPassword = await saltAndHashPassword(password);
-
-  return await dbUserCreate(user, hashedPassword);
-};
